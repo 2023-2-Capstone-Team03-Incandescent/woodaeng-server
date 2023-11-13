d31f96867d56f67de0ad9ba5cd485bc542a790ed
tthisag246 <tthisag246@gmail.com>
2023-11-13 23:42:29 +0900

    setAllowedOrigins

--- src/main/java/com/incandescent/woodaengserver/config/WebSocketConfig.java
+++ src/main/java/com/incandescent/woodaengserver/config/WebSocketConfig.java
@@ -10,7 +10,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("http://www.woodaeng.kro.kr:8080");
     }
 
     @Override
