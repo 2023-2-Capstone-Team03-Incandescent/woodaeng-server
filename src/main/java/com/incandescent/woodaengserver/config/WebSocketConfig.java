ebd2d9cc0eb8e8f307ce4acf4d2ec7c381bb1282
tthisag246 <tthisag246@gmail.com>
2023-11-13 23:04:38 +0900

    without SockJS

--- src/main/java/com/incandescent/woodaengserver/config/WebSocketConfig.java
+++ src/main/java/com/incandescent/woodaengserver/config/WebSocketConfig.java
@@ -10,8 +10,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
-                .withSockJS();
+                .setAllowedOrigins("*");
     }
 
     @Override
