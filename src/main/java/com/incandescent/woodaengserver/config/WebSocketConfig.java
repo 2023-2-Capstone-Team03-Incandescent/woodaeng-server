6a38c1203aa2c19137d855babdddfadb57119fb3
tthisag246 <tthisag246@gmail.com>
2023-11-14 14:59:33 +0900

    sock

--- src/main/java/com/incandescent/woodaengserver/config/WebSocketConfig.java
+++ src/main/java/com/incandescent/woodaengserver/config/WebSocketConfig.java
@@ -11,8 +11,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("app://*", "https://*")
-                .withSockJS();
+                .setAllowedOriginPatterns("app://*", "https://*");
     }
 
     @Override
